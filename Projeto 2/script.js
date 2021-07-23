016ce01ada031106872f5e76a7f307e617d4efc2
anacarlacpp <76544453+anacarlacpp@users.noreply.github.com>
2021-07-23 19:33:53 -0300

    Adc 1

---
+++ Projeto 2/script.js
@@ -0,0 +1,10 @@
+const listconteiner = document.querySelector('[data-list]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+let list = []
+
+newListForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    
+}
